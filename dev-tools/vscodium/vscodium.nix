0221c4fb6c35da8d530af5cf3a1240a615987633
phv <hvprithvi09@gmail.com>
2021-02-10 17:05:56 +0530

    feat(vscode-pkgs): added some packages

--- dev-tools/vscodium/vscodium.nix
+++ dev-tools/vscodium/vscodium.nix
@@ -17,44 +17,44 @@ let
     ]);
 
     themeExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-	   name = "theme-draula";
-	   publisher = "dracula-theme";
-           version = "2.22.3";
-           sha256 = "869f334c553ae9bb7f64af1aed599019753729325a789a5164a4d818b34ed172";
-	}      
+            name = "theme-dracula";
+            publisher = "dracula-theme";
+            version = "2.22.3";
+            sha256 = "869f334c553ae9bb7f64af1aed599019753729325a789a5164a4d818b34ed172";
+	    }    
     ]);
 
     elixirExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-	   name = "elixir-ls";
-	   publisher = "jakebecker";
-           version = "0.6.5";
-           sha256 = "e0ba6589639db8c9e96caddb714ac741f5594bcfd49b308c0e5625a83c80f8c8"; 
-	}
+            name = "elixir-ls";
+            publisher = "jakebecker";
+            version = "0.6.5";
+            sha256 = "e0ba6589639db8c9e96caddb714ac741f5594bcfd49b308c0e5625a83c80f8c8"; 
+        }
     ]);
 
     erlangExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-	   name = "erlang";
-	   publisher = "pgourlain";
-           version = "0.6.9";
-           sha256 = "6681ba74a65c0463ae8bb2d3105812fe432533b8e795688476a713e4f1766f7d"; 
-	}
+            name = "erlang";
+            publisher = "pgourlain";
+            version = "0.6.9";
+            sha256 = "6681ba74a65c0463ae8bb2d3105812fe432533b8e795688476a713e4f1766f7d"; 
+        }
     ]);
 
     elmExt = (with pkgs.vscode-extensions;[
-	elmtooling
+	    elmtooling.elm-ls-vscode
     ]);
     
     fishExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-	   name = "vscode-fish";
-	   publisher = "bmalehorn";
-           version = "1.0.20";
-           sha256 = "684e312fdb4f8f55360768be98c58a36049a856a41db8faf1d85eb53dc11071a"; 
-	}
+            name = "vscode-fish";
+            publisher = "bmalehorn";
+            version = "1.0.20";
+            sha256 = "684e312fdb4f8f55360768be98c58a36049a856a41db8faf1d85eb53dc11071a"; 
+        }
     ]);
 
 
     GolangExt = (with pkgs.vscode-extensions; [
-	    golang
+	    golang.Go
     ]);
 
     protoExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
@@ -65,30 +65,36 @@ let
 	    }
         {
             name = "vscode-proto3";
-            publisher = "zsh404";
+            publisher = "zxh404";
             version = "0.5.3";
             sha256 = "a144a287e61d0176244cd7a36412637177b041ec2040530684015d27f08131de";
         }
     ]);
 
     gitExt = (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-	   name = "gitlens";
-	   publisher = "gitlens";
-           version = "11.2.1";
-           sha256 = "6613130a1968a14d4eb0edb83965db529a8b7ad8ab534c7f072ced7ab21647ad";
-	}
+            name = "gitlens";
+            publisher = "eamodio";
+            version = "11.2.1";
+            sha256 = "6613130a1968a14d4eb0edb83965db529a8b7ad8ab534c7f072ced7ab21647ad";
+        }
     ]);
 
     misc = (with pkgs.vscode-extensions; [
-       WakaTime
+       WakaTime.vscode-wakatime
     ]);
 
-    allExtensions = nixExt;
-    #    nixExt 
-        # ++ dockerExt
-	    # ++ misc;
-        # ++ GolangExt;
-        # ++ dataFileFormatsExt;
+    allExtensions = 
+       nixExt 
+	    ++ misc
+        ++ dataFileFormatsExt
+        ++ protoExt
+        ++ themeExt
+        ++ gitExt
+        ++ dockerExt
+        ++ fishExt
+        ++ GolangExt
+        ++ elixirExt
+        ++ erlangExt;
 in {
     enable = true;
     package = pkgs.vscodium;
