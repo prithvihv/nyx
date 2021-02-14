c56e3c50b0cc0895f1eeca8c2a8749bd3871be46
phv <hvprithvi09@gmail.com>
2021-02-14 15:30:53 +0530

    feat(nvim): added nvim config

---
+++ dev-tools/nvim/config.vim
@@ -0,0 +1,8 @@
+set noswapfile
+set nobackup
+set tabstop=4
+set number
+set relativenumber
+set wildmenu
+
+colorscheme dracula
--- dev-tools/nvim/nvim.nix
+++ dev-tools/nvim/nvim.nix
@@ -5,4 +5,36 @@ in {
   viAlias = true;
   vimAlias = true;
   vimdiffAlias = true;
+
+  extraPackages = with pkgs;[
+    git
+    vimPlugins.vim-plug
+  ];
+
+  configure  = {
+    
+    customRC = builtins.readFile ./config.vim ;
+
+    plug.plugins = with pkgs.vimPlugins; [
+      # misc
+      dracula-vim
+      YouCompleteMe
+      vim-wakatime
+      vim-watchdogs
+      vim-lastplace
+    ] ++ [
+      # language support
+      rust-vim
+      elm-vim
+      vim-go
+      vim-nix
+      vim-elixir
+      vim-fish
+      vim-javascript
+    ] ++ [
+      # data representations
+      vim-json
+      vim-yaml
+    ];
+  };
 }
