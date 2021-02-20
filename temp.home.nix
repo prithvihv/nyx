abdbc7d1b76d9eae6b50d03345b2c88892e03e06
phv <hvprithvi09@gmail.com>
2021-02-20 09:01:07 +0000

    feat(compositor): added picom, taffy lookgs great

--- temp.home.nix
+++ temp.home.nix
@@ -1,19 +1,23 @@
 { config, pkgs, ... }:
 
 let 
-    xsessionPhv = import ../../nix-config/xsession.nix { inherit pkgs; };
-    nvimConfig = import ../../nix-config/dev-tools/nvim/nvim.nix { inherit pkgs; };
-    haskellDev = import ../../nix-config/dev-tools/haskell/pkgs.nix { inherit pkgs; };
-    alacrittyConfig = with pkgs;import ../../nix-config/alacritty/alacritty.nix { 
+    xsessionPhv = import ../../nyx/xsession.nix { inherit pkgs; };
+    nvimConfig = import ../../nyx/dev-tools/nvim/nvim.nix { inherit pkgs; };
+    haskellDev = import ../../nyx/dev-tools/haskell/pkgs.nix { inherit pkgs; };
+    alacrittyConfig = with pkgs;import ../../nyx/alacritty/alacritty.nix { 
       critty = alacritty; 
       inherit fish;
     };
-    taffyConfig = import ../../nix-config/taffybar-phv/taffybar.nix { inherit pkgs; };
-    vscodium = import ../../nix-config/dev-tools/vscodium/vscodium.nix { inherit pkgs; };
+    taffyConfig = import ../../nyx/taffybar-phv/taffybar.nix { inherit pkgs; };
+    vscodium = import ../../nyx/dev-tools/vscodium/vscodium.nix { inherit pkgs; };
 in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
   # get neovim-nightly
   nixpkgs.overlays = [
@@ -21,11 +25,6 @@ in
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
     }))
   ];
-
-  # BUT WHY?
-  home.file.".config/nixpkgs/config.nix".text = ''
-  	{ allowUnfree = true; }
-  '';
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -43,7 +42,9 @@ in
     # for taffy
     # TODO: move this to taffy.nix
     at-spi2-core # https://gist.github.com/jeffcogswell/62395900725acef1c0a5a608f7eb7a05
-    hicolor-icon-theme
+    # hicolor-icon-theme
+
+    jetbrains-mono
   ];
 
 
@@ -60,19 +61,22 @@ in
   # services.picom = {
   #   enable = true;
   # };
-  # services.picom = {
-  #   enable = true;
-  #   blur = true;
-  #   package = pkgs.picom;
-  #   # activeOpacity = "1.0";
-  #   # inactiveOpacity = "0.8";
-  #   backend = "glx";
-  #   # fade = true;
-  #   # fadeDelta = 5;
-  #   # opacityRule = [ "100:name *= 'i3lock'" ];
-  #   # shadow = true;
-  #   # shadowOpacity = "0.75";
-  # };
+  # FIXME: enabling is mnaual
+  # systemctl --user start picom.service
+  # systemctl --user enable picom.service
+  services.picom = {
+    enable = true;
+    blur = true;
+    package = pkgs.picom;
+    # activeOpacity = "1.0";
+    # inactiveOpacity = "0.8";
+    backend = "glx";
+    # fade = true;
+    # fadeDelta = 5;
+    # opacityRule = [ "100:name *= 'i3lock'" ];
+    # shadow = true;
+    # shadowOpacity = "0.75";
+  };
 
   # This value determivscodiumnes the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
--- xmonad-phv/Main.hs
+++ xmonad-phv/Main.hs
@@ -16,11 +16,6 @@ import XMonad.Layout.NoBorders(smartBorders)
 myTerminal = "alacritty"
 myGUILuncher = "rofi -show drun"
 
-myWorkspaces =
-  ["1:term", "2:web", "3:code", "4:comms", "5:web2.0"]
-    ++ map show [6 .. 8]
-    ++ ["9:config"]
-
 -- xmonad
 myBorderWidth = 1
 myNormalBorderColor = "#000000"
@@ -37,11 +32,11 @@ myKeys baseConfig@XConfig {XMonad.modMask = modMask} =
         , ((modMask, xK_a), spawn "screenshot")
       ] <> keys def baseConfig
 
+-- , workspaces = myWorkspaces
 main = xmonad . docks . ewmh $ def
   { modMask = mod4Mask  
   , terminal = myTerminal
   , borderWidth = myBorderWidth
-  , workspaces = myWorkspaces
   , keys = myKeys
   , layoutHook = avoidStruts $ (smartBorders $ (layoutHook def))
   , manageHook = manageDocks
