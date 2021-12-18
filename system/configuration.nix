df24a7a5ac02359decf690ba6e4ceb20298f9948
prithvihv <hvprithvi09@gmail.com>
2021-12-18 20:39:42 +0530

    fix: postgres

--- system/configuration.nix
+++ system/configuration.nix
@@ -84,7 +84,8 @@
     enable = true;
     package = pkgs.postgresql_13;
     authentication = pkgs.lib.mkOverride 10 ''
-      local  all      all          trust
+      local all all trust
+      host    all             all             127.0.0.1/32            trust
     '';
   };
 
