468afc267376dd268a2fbea7643227fe8000e8f5
prithvihv <hvprithvi09@gmail.com>
2022-08-13 05:46:28 +0530

    chore: clean up

--- system/configuration.nix
+++ system/configuration.nix
@@ -140,18 +140,6 @@ in {
   };
   services.hardware.bolt.enable = true;
 
-  # https://wiki.archlinux.org/title/microcode
-  # hardware.cpu.intel.updateMicrocode = true;
-
-  # hardware.nvidia.prime = {
-  #   offload.enable = true;
-
-  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-  #   intelBusId = "PCI:0:2:0";
-
-  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-  #   nvidiaBusId = "PCI:1:0:0";
-  # };
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
