3608318fc27f2a21771a3fcdc34526565846eb6e
prithvihv-wooga <prithvi.virupaksha@wooga.net>
2024-04-05 20:19:39 +0200

    format homebrew file

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -2,16 +2,21 @@
   homebrew = {
     enable = true;
     onActivation.upgrade = true;
-    brews = [ "saml2aws" "asdf"] ++ [
+    brews = [ "saml2aws" "asdf" ] ++ [
       # needs these for erlang asdf
       # https://github.com/asdf-vm/asdf-erlang#osx
-      "ncurses" "autoconf" "wxwidgets" "libxslt" "fop"
+      "ncurses"
+      "autoconf"
+      "wxwidgets"
+      "libxslt"
+      "fop"
     ];
     # updates homebrew packages on activation,
     # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
     casks = [ "gpg-suite" ] ++ [
       "vlc"
       "signal"
+      "whatsapp"
       "chromium"
       "spotify"
       # "alfred"
@@ -22,17 +27,17 @@
       "amethyst"
       "anki"
       "logseq"
-    ] ++ [ 
+    ] ++ [
       # devtools GUIs
-      "postman" 
-      "lens" 
+      "postman"
+      "lens"
       "datagrip"
     ] ++ [
       "android-platform-tools"
       "android-file-transfer"
 
       # aws session manager-plugin 
-      "session-manager-plugin" 
+      "session-manager-plugin"
     ];
   };
 }
