f337ad86cd728d4be834fc442c281e6211aadcef
prithvihv <hvprithvi09@gmail.com>
2025-10-05 06:38:45 +0200

    configure git on mbp-m4

--- darwin/mbp-m4/home.nix
+++ darwin/mbp-m4/home.nix
@@ -1,11 +1,13 @@
 { pkgs, lib, ... }:
 let
 fishConfig = import ../../pkgs/fish.nix { inherit pkgs lib; isWoogaMachine = false; };
+gitConfig = import ../../pkgs/git.nix { inherit pkgs; };
 in
 {
   programs.bash.enable = true;
   programs.zsh.enable = true;
   programs.fish = fishConfig;
+  programs.git = gitConfig;
 
   # The state version is required and should stay at the version you
   # originally installed.
