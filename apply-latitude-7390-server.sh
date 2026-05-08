144212e73f7d81e54ebf7cece97701388a311a9b
prithvihv <hvprithvi09@gmail.com>
2026-05-09 01:32:24 +0200

    Use script directory instead of hardcoded path
    Replace the hardcoded `~/.nyx` path with a dynamic
    reference to the script's directory, making the script
    more portable and easier to relocate.

--- apply-latitude-7390-server.sh
+++ apply-latitude-7390-server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pushd ~/.nyx
+DIR="$(cd "$(dirname "$0")" && pwd)"
+pushd "$DIR"
 git add .
 # sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
 sudo nixos-rebuild switch --flake .#dell-latitude-7390-server
