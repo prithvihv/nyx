31398e6627529e5e11e3815a8265f07b10981b9c
prithvihv <hvprithvi09@gmail.com>
2026-05-09 11:14:24 +0200

    Change woodpecker home directory to /var/empty

--- system/dell-latitude-7390-server/woodpecker-home.nix
+++ system/dell-latitude-7390-server/woodpecker-home.nix
@@ -2,7 +2,7 @@
 
 {
   home.stateVersion = "25.11";
-  home.homeDirectory = "/var/lib/woodpecker";
+  home.homeDirectory = "/var/empty";
 
   # Tools available to the woodpecker CI agent.
   # Add packages here as needed.
