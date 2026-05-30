6b8154800b944c4cfb5cceb5f6afd30b723ccc0b
prithvihv <hvprithvi09@gmail.com>
2026-05-30 03:40:31 +0200

    Add openspec to mbp-m4 Home Manager

--- flake.nix
+++ flake.nix
@@ -7,8 +7,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows =
-      "nixpkgs"; # ask hm to use pinned nixpkgs
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # ask hm to use pinned nixpkgs
 
     # darwin stuff
     darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
@@ -19,13 +18,21 @@
     elixir-extra.inputs.nixpkgs.follows = "nixpkgs";
 
     # sbs
-   sbs.url =
-     "git+ssh://git@github.com/wooga/sbs-nix.git";
-   sbs.inputs.nixpkgs.follows = "nixpkgs";
+    sbs.url = "git+ssh://git@github.com/wooga/sbs-nix.git";
+    sbs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, sops-nix, darwin, elixir-extra
-    , nixos-unstable, sbs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      sops-nix,
+      darwin,
+      elixir-extra,
+      nixos-unstable,
+      sbs,
+    }:
     let
       system = "x86_64-linux";
       insecurePakages = [
@@ -41,71 +48,71 @@
         };
         overlays = [ elixir-extra.overlay ];
       };
-    in {
-      darwinConfigurations.mw-pvirupa-GK4K = let
-        system = "aarch64-darwin";
-        darwin-unstable-nixpkgs = import nixos-unstable {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            permittedInsecurePackages = insecurePakages;
-          };
+
+      # Shared nixpkgs configuration for both Macs (mb-wooga + mbp-m4).
+      darwinSystem = "aarch64-darwin";
+      darwin-unstable-nixpkgs = import nixos-unstable {
+        system = darwinSystem;
+        config = {
+          allowUnfree = true;
+          permittedInsecurePackages = insecurePakages;
         };
-        darwin-nixpkgs = {
-          config = {
-            allowUnfree = true;
-            allowUnsupportedSystem = true;
-            permittedInsecurePackages = [ "nodejs-14.21.3" "openssl-1.1.1u" ];
-          };
-          # overlays = [
-          #   (final: prev: { terraform = darwin-unstable-nixpkgs.terraform; })
-          # ];
+      };
+      darwin-nixpkgs = {
+        config = {
+          allowUnfree = true;
+          allowUnsupportedSystem = true;
+          permittedInsecurePackages = [
+            "nodejs-14.21.3"
+            "openssl-1.1.1u"
+          ];
         };
-      in darwin.lib.darwinSystem {
-        inherit system;
-        modules = [
-          ./darwin/mb-wooga/configuration.nix
-          ./darwin/mb-wooga/homebrew.nix
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = darwin-nixpkgs;
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."prithvi.virupaksha" = import ./darwin/mb-wooga/home.nix;
-           home-manager.extraSpecialArgs = { sbs = sbs.packages.${system}; };
-          }
+        overlays = [
+          (final: prev: { openspec = darwin-unstable-nixpkgs.openspec; })
         ];
       };
-
-      darwinConfigurations.mbp-m4 = let system = "aarch64-darwin";
-        darwin-nixpkgs = {
-          config = {
-            allowUnfree = true;
-            allowUnsupportedSystem = true;
-            # permittedInsecurePackages = [ "nodejs-14.21.3" "openssl-1.1.1u" ];
-          };
+    in
+    {
+      darwinConfigurations = {
+        mw-pvirupa-GK4K = darwin.lib.darwinSystem {
+          system = darwinSystem;
+          modules = [
+            ./darwin/mb-wooga/configuration.nix
+            ./darwin/mb-wooga/homebrew.nix
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs = darwin-nixpkgs;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."prithvi.virupaksha" = import ./darwin/mb-wooga/home.nix;
+              home-manager.extraSpecialArgs = {
+                sbs = sbs.packages.${darwinSystem};
+              };
+            }
+          ];
         };
-      in darwin.lib.darwinSystem {
-        inherit system;
-        modules = [
-          ./darwin/mbp-m4/configuration.nix
-          {system.configurationRevision = self.rev or self.dirtyRev or null;}
-          # ./darwin/mbp-m4/homebrew.nix
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = darwin-nixpkgs;
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."phv" = import ./darwin/mbp-m4/home.nix;
-          }
-        ];
+
+        mbp-m4 = darwin.lib.darwinSystem {
+          system = darwinSystem;
+          modules = [
+            ./darwin/mbp-m4/configuration.nix
+            { system.configurationRevision = self.rev or self.dirtyRev or null; }
+            # ./darwin/mbp-m4/homebrew.nix
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs = darwin-nixpkgs;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."phv" = import ./darwin/mbp-m4/home.nix;
+            }
+          ];
+        };
       };
 
       nixosConfigurations = {
-        work-station = let pkgs = linux-nixpkgs;
-        in lib.nixosSystem {
+        work-station = lib.nixosSystem {
           inherit system;
-          inherit pkgs;
+          pkgs = linux-nixpkgs;
           modules = [
             ./system/work-station/configuration.nix
             home-manager.nixosModules.home-manager
@@ -117,10 +124,9 @@
           ];
         };
 
-        dell-latitude-7390 = let pkgs = linux-nixpkgs;
-        in lib.nixosSystem {
+        dell-latitude-7390 = lib.nixosSystem {
           inherit system;
-          inherit pkgs;
+          pkgs = linux-nixpkgs;
           modules = [
             ./system/dell-latitude-7390/configuration.nix
             home-manager.nixosModules.home-manager
@@ -133,10 +139,9 @@
           ];
         };
 
-        dell-latitude-7390-server = let pkgs = linux-nixpkgs;
-        in lib.nixosSystem {
+        dell-latitude-7390-server = lib.nixosSystem {
           inherit system;
-          inherit pkgs;
+          pkgs = linux-nixpkgs;
           modules = [
             ./system/dell-latitude-7390-server/configuration.nix
             home-manager.nixosModules.home-manager
