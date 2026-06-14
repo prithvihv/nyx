6970ee29f640179de84628512ddeba9b27c9c17b
prithvihv <hvprithvi09@gmail.com>
2026-06-14 16:36:48 +0200

    Update Caddy Cloudflare plugin hash

--- system/dell-latitude-7390-server/ingress.nix
+++ system/dell-latitude-7390-server/ingress.nix
@@ -113,7 +113,7 @@ in
 
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
+      hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
     };
 
     globalConfig = ''
